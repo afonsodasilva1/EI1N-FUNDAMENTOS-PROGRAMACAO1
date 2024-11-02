fab1da3d0fb7ce54cac70f5f9b88a815dc2352c4
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-11-02 19:32:22 +0100

    Update python.py

--- python/python.py
+++ python/python.py
@@ -1,1 +1,10 @@
-print ("Hello, World!")
+
+# Um Funcionário sofreu um aumento de sobre o seu salário base e deseja saber qual será o seu salário final sabendo que paga um importo de 5%
+
+SB = (float(input("Digite o seu salário")))
+
+aumento = SB * 30 / 100
+
+imposto = SB * 5 / 100
+
+salario_final = SB + aumento - imposto
