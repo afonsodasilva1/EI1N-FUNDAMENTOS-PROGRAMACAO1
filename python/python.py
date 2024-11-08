8bc4024501e9f23dd09b7b552d4194de9c10d245
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-11-08 19:02:30 +0100

    Update python.py

--- python/python.py
+++ python/python.py
@@ -2,9 +2,20 @@
 #print('ontem', 'Hoje', 'Amanhã', sep='-')
 #print('B', 'n', 'n', '.', sep='a')
 
-mensalidade = 75000
-curso = "Engenharia Informática"
+#mensalidade = float (input("Digite a mensalidade: "))
+
+
+
+#mensalidade = float(mensalidade)
+#curso = "Engenharia Informática"
 #print("A mensalidade do curso de %s é de %d kwanzas"%(curso,mensalidade))
 
 
-print("A mensalidade de %+10s é %5.2d kwanzas"%(curso,mensalidade))
+#print("A mensalidade de %+10s é %2.2f kwanzas"%(curso,mensalidade))
+
+
+num1 = int (input("Digite um número: "))
+num2 = int (input("Digite um número: "))
+
+print(f'A divsão de {num1} por {num2} é: {num1*num2}')
+
