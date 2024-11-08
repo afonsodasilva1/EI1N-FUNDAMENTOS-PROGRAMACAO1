d6b7721d73d090d112c80e627445009d9f4097ca
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-11-09 00:22:41 +0100

    Update python.py

--- python/python.py
+++ python/python.py
@@ -19,3 +19,97 @@ num2 = int (input("Digite um número: "))
 
 print(f'A divsão de {num1} por {num2} é: {num1*num2}')
 
+#"Crie 3 variaveis com 3 tipos de dados diferente"
+
+#nome =  'Afonso'
+#AnO = 2024
+#valor = 19.99
+
+"Cria um comentário de mas d uma linha"
+#Exemplo de um comentário simples, de até uma linha"
+
+
+'''3 - Crie comentário de mais de uma linha
+aoaoosasa
+Palvaa
+asasasasa
+'''
+
+
+"4"
+print("Olá Mundo")
+
+"5"
+
+#nome = input('Por favor digite o seu nome')
+
+'''"6 Exi em tela o valor e o tipo de variável num1: Sendo num1 = 1987"
+num1 = 1987
+print(num1)
+print(type (num1))
+
+
+"7"
+
+num1 = input ("Digite um número: ")
+print(f'O número digitaado é: {num}')
+
+"8 "
+
+num = input("Digite um número: ")
+num = float(num)
+
+print (num)
+
+print(type(num))
+
+" 9 - Crie uma lista com cinco nome de pessoas"
+
+nomes = ['Ana', 'Carlos', 'Diane', 'Fernando', 'Maria']
+
+print(nomes)'''
+
+''' "10 .Mostre o tamnho "
+
+nomes = ['Ana', 'Carlos', 'Diane', 'Fernando', 'Maria']
+print(len(nomes))
+
+print(nomes[3]) '''
+
+'''
+"11 -  Some os valores das váriaveis num1 e num2: Sendo num1 = 52 e num2 = 106"
+
+num1 = 52
+num2 = 106
+
+print(num1 +num2)
+
+
+"12 -  Some os valores das variaveis num1 e num2, atribuindo o re"
+
+num1 = 52
+num2 = 106
+soma = num1 +num2
+print(soma)
+
+"13"
+
+num1 = 52
+num2 = 106
+print(num1 - num2)
+
+#ou
+
+sutracao = num1 - num2
+print(sutracao)
+
+
+
+"14"
+
+num1 = 52
+num2 = 106
+
+print(num1 * num2)
+print(num1 / num2)
+'''
