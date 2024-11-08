1e0cc5e447e8a9b126fef86700ce59ed09a09e87
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-11-08 18:35:04 +0100

    Update python.py

--- python/python.py
+++ python/python.py
@@ -1,3 +1,10 @@
-print('Dia', 'Mês', 'Ano', sep='/')
-print('ontem', 'Hoje', 'Amanhã', sep='-')
-print('B', 'n', 'n', '.', sep='a')
+#print('Dia', 'Mês', 'Ano', sep='/')
+#print('ontem', 'Hoje', 'Amanhã', sep='-')
+#print('B', 'n', 'n', '.', sep='a')
+
+mensalidade = 75000
+curso = "Engenharia Informática"
+#print("A mensalidade do curso de %s é de %d kwanzas"%(curso,mensalidade))
+
+
+print("A mensalidade de %+10s é %5.2d kwanzas"%(curso,mensalidade))
